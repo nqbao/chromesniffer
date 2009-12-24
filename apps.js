050a15c08527ed47af104324f93a0cd477e23484
bao <contact@nqbao.com>
2009-12-24 17:20:39 +0700

    basic detecting system

--- apps.js
+++ apps.js
@@ -1,6 +1,16 @@
-var apps = {
-	'': {
+var appinfo = {
+	'': { // default
+		icon: 'unknown.jpg',
+		url: 'http://google.com/search?q=%s'
+	},
+	'Drupal': {
+		icon: 'Drupal.ico',
+		url: 'http://drupal.org'
+	},
+	'Google Analytics': {
+		icon: 'Google_Analytics.ico'
 	},
 	'jQuery': {
+		icon: 'jQuery.ico'
 	}
 };
