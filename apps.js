c8cbd3d4ca76432dfc75ded2f6be43ce39580081
bao <contact@nqbao.com>
2009-12-24 17:27:45 +0700

    add wordpress, joomla, vBulletin to app list, enhance app info storage

--- apps.js
+++ apps.js
@@ -8,9 +8,22 @@ var appinfo = {
 		url: 'http://drupal.org'
 	},
 	'Google Analytics': {
-		icon: 'Google_Analytics.ico'
+		icon: 'Google_Analytics.ico',
+		url: 'http://www.google.com/analytics/'
 	},
 	'jQuery': {
 		icon: 'jQuery.ico'
+	},
+	'WordPress': {
+		icon: 'WordPress.ico',
+		url: 'http://wordpress.org'
+	},
+	'Joomla': {
+		icon: 'Joomla.ico',
+		url: 'http://joomla.org'
+	},
+	'vBulletin' {
+		icon: 'vBulletin.ico',
+		url: 'http://www.vbulletin.com/'
 	}
 };
