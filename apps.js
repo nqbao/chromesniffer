ea3f3240fde8a6dac15f99f4ffc74900f07494e4
bao <contact@nqbao.com>
2009-12-29 14:17:40 +0700

    re-structure app definition file, add some more apps

--- apps.js
+++ apps.js
@@ -8,63 +8,168 @@
  **/
 
 var appinfo = {
-	'': { // default
-		icon: 'unknown.jpg',
-		url: 'http://google.com/search?q=%s'
+	// ==== services ====
+	'Tumblr': {
+		icon: 'Tumblr.ico',
+		url: 'http://tumblr.com',
+		priority: 0.9
 	},
+
+	// ==== cms or framework ====
+	'vBulletin': {
+		icon: 'vBulletin.ico',
+		url: 'http://www.vbulletin.com/',
+		priority: 1
+	},
+	'SMF': {
+		title: 'Simple Machines Forum',
+		icon: 'SMF.ico',
+		url: 'http://www.simplemachines.org/',
+		priority: 1
+	},
+	'phpBB': {
+		icon: 'phpBB.ico',
+		url: 'http://phpbb.org',
+		priority: 1
+	},
+	'IPB': {
+		title: 'Invision Power Board',
+		icon: 'IPB.ico',
+		url: 'http://invisionpower.com/',
+		priority: 1
+	},
+	
 	'Drupal': {
 		icon: 'Drupal.ico',
 		url: 'http://drupal.org',
 		priority: 1
 	},
-	'Google Analytics': {
-		icon: 'Google_Analytics.ico',
-		url: 'http://www.google.com/analytics/'
+	'Ubercart': {
+		icon: 'Ubercart.ico',
+		url: 'http://www.ubercart.org/',
+		priority: 0.9 // higher priority compared with Drupal
 	},
-	'jQuery': {
-		icon: 'jQuery.ico'
-	},
+	
 	'WordPress': {
 		icon: 'WordPress.ico',
 		url: 'http://wordpress.org',
 		priority: 1
 	},
+	'Movable Type': {
+		icon: 'MovableType.ico',
+		url: 'http://www.movabletype.org/',
+		priority: 1
+	},
+	
 	'Joomla': {
 		icon: 'Joomla.ico',
 		url: 'http://joomla.org',
 		priority: 1
 	},
-	'vBulletin': {
-		icon: 'vBulletin.ico',
-		url: 'http://www.vbulletin.com/'
-	},
-	'Prototype': {
-		icon: 'Prototype.ico',
-		url: 'http://www.prototypejs.org/'
-	},
-	'SMF': {
-		title: 'Simple Machines Forum',
-		icon: 'SMF.ico',
-		url: 'http://www.simplemachines.org/'
-	},
-	'phpBB': {
-		icon: 'phpBB.ico',
-		url: 'http://phpbb.org'
-	},
 	'MediaWiki': {
 		icon: 'MediaWiki.ico',
-		url: 'http://www.mediawiki.org/'
+		url: 'http://www.mediawiki.org/',
+		priority: 1
 	},
 	'Magento': {
 		icon: 'Magento.ico',
-		url: 'http://www.magentocommerce.com/'
+		url: 'http://www.magentocommerce.com/',
+		priority: 1
 	},
 	'XOOPS': {
 		icon: 'XOOPS.ico',
-		url: 'http://xoops.org'
+		url: 'http://xoops.org',
+		priority: 1
 	},
-	'Ubercart': {
-		icon: 'Ubercart.ico',
-		url: 'http://www.ubercart.org/'
+	'Plone': {
+		icon: 'Plone.ico',
+		url: 'http://plone.org/',
+		priority: 1
+	},
+	'CMSMadeSimple': {
+		title: 'CMS Made Simple',
+		icon: 'CMSMadeSimple.ico',
+		url: 'http://www.cmsmadesimple.org/',
+		priority: 1
+	},
+	'SilverStripe': {
+		icon: 'SilverStripe.png',
+		url: 'http://silverstripe.org',
+		priority: 1
+	},
+	'MODx': {
+		icon: 'MODx.ico',
+		url: 'http://modxcms.com/',
+		priority: 1
+	},
+	'Amiro.CMS': {
+		icon: 'Amiro.CMS.ico',
+		url: 'http://www.amirocms.com/',
+		priority: 1
+	},
+	'Koobi': {
+		icon: 'koobi.ico',
+		url: 'http://www.dream4.de/cms/',
+		priority: 1
+	},
+	
+	// ==== js framework ====
+	'jQuery': {
+		icon: 'jQuery.ico',
+		url: 'http://jquery.com',
+		priority: 2
+	},
+	'jQuery UI': {
+		icon: 'jQuery_UI.ico',
+		url: 'http://jqueryui.com/',
+		priority: 3
+	},
+	'ExtJS': {
+		icon: 'ExtJS.ico',
+		url: 'http://www.extjs.com/',
+		priority: 2
+	},
+	'Prototype': {
+		icon: 'Prototype.ico',
+		url: 'http://www.prototypejs.org/',
+		priority: 2
+	},
+	'Closure': {
+		icon: 'Closure.png',
+		url: 'http://code.google.com/closure/',
+		priority: 2
+	},
+	'MooTools': {
+		icon: 'MooTools.ico',
+		url: 'http://mootools.net/',
+		priority: 2
+	},
+	'Dojo': {
+		icon: 'Dojo.ico',
+		url: 'http://www.dojotoolkit.org/',
+		priority: 2
+	},
+	'script.aculo.us': {
+		icon: 'script.aculo.us.ico',
+		url: 'http://script.aculo.us/',
+		priority: 1.9
+	},
+	'YUI': {
+		icon: 'YUI.ico',
+		url: 'http://developer.yahoo.com/yui/',
+		priority: 2
+	},
+	
+	// ==== js tools ====
+	
+	'Google Analytics': {
+		icon: 'Google_Analytics.ico',
+		url: 'http://www.google.com/analytics/'
+	},
+	
+	// ==== misc ====
+	'': { // default
+		icon: 'unknown.jpg',
+		url: 'http://google.com/search?q=%s'
 	}
 };
