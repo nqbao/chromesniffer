7399f1aac7e2314769c2c7c1e2221946ed03ac2e
Adam Nemecek <adamnemecek@gmail.com>
2013-01-05 20:24:43 -0800

    Merged JavaScript presence tests with the version tests. Split popup.html between popup.html and popup.js. Fixed style.

--- main.js
+++ main.js
@@ -27,7 +27,7 @@
 		if (meta) {
 			var apps = JSON.parse(meta.content)
 			if (Object.keys(apps).length > 0) {
-				chrome.extension.sendMessage({msg: "result",apps: apps});
+				chrome.extension.sendMessage({msg: "result", apps: apps});
 			}
 		}
 	});
