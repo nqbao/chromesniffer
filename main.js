efde54c0371aba14201fd15b9e8c7fe7057dc6d2
buihoangvu <buihoangvu@gmail.com>
2013-10-04 16:04:07 +0700

    fix chromesniffer_meta - undefined meta object

--- main.js
+++ main.js
@@ -8,27 +8,26 @@
  * @license GPLv3
  **/
 
-(function(){
-	var head = document.getElementsByTagName('head')[0];
+(function () {
+  var head = document.getElementsByTagName('head')[0];
 
-	if (head) {
-		var script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.src = chrome.extension.getURL('detector.js');
+  if (head) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = chrome.extension.getURL('detector.js');
 
-		var meta = document.createElement('meta');
-		meta.name = 'chromesniffer';
-		meta.id = 'chromesniffer_meta';
-		head.appendChild(meta);
-		head.appendChild(script);
-	}
-	meta.addEventListener('ready', function(){
-		if (meta) {
-			var apps = JSON.parse(meta.content)
-			
-			if (Object.keys(apps).length > 0) {
-				chrome.extension.sendRequest({msg: "result",apps: apps});
-			}
-		}
-	});
+    var meta = document.createElement('meta');
+    meta.name = 'chromesniffer';
+    meta.id = 'chromesniffer_meta';
+    head.appendChild(meta);
+    head.appendChild(script);
+
+    meta.addEventListener('ready', function () {
+      var apps = JSON.parse(meta.content);
+
+      if (Object.keys(apps).length > 0) {
+        chrome.extension.sendRequest({msg: "result", apps: apps});
+      }
+    });
+  }
 })();
